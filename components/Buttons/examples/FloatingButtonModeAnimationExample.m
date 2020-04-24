4d7e5a57140e3ad978394a7596c7635b110ebff7
Jeff Verkoeyen <featherless@google.com>
2020-04-23 21:49:33 -0400

    Rename all icons to an ic_<#name#> convention.
    PiperOrigin-RevId: 308174251

--- components/Buttons/examples/FloatingButtonModeAnimationExample.m
+++ components/Buttons/examples/FloatingButtonModeAnimationExample.m
@@ -41,7 +41,7 @@ static NSString *const kButtonLabel = @"Create";
   self.view.backgroundColor = [UIColor colorWithWhite:0.9f alpha:1];
 
   UIImage *plusImage =
-      [[UIImage imageNamed:@"Plus"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+      [[UIImage imageNamed:@"ic_add"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
   UIImage *plusImage36 = [UIImage imageNamed:@"plus_white_36"
                                     inBundle:[NSBundle bundleForClass:[self class]]
                compatibleWithTraitCollection:self.traitCollection];
